7e229fd92282b458b0115dba8ebb44dbc52921d8
J. Francisco Raposerias <rapofran@gmail.com>
2015-08-13 10:31:29 -0300

    Added drum sequencer, and fix fx.html error

---
+++ js/beatSequence.js
@@ -0,0 +1,66 @@
+if (!window.AudioContext) alert('you browser doesnt support Web Audio API')
+
+var soundBank = {}
+  , beats = {}
+  , tempo = QUERY.tempo || 200
+  , signature = QUERY.signature || 7
+  , beatDur = 60/tempo
+  , barDur = signature * beatDur
+  , clock, context, uiEvent
+
+$('#startButton').click(function() {
+  context = new AudioContext()
+  clock = new WAAClock(context, {toleranceEarly: 0.1})
+  clock.start()
+
+  // The following code highlights the current beat in the UI by calling the function `uiNextBeat` periodically.
+  uiEvent = clock.callbackAtTime(uiNextBeat, nextBeatTime(0))
+    .repeat(beatDur)
+    .tolerance({late: 100})
+})
+
+// This function activates the beat `beatInd` of `track`.
+var startBeat = function(track, beatInd) {
+  var event = clock.callbackAtTime(function(event) {
+    var bufferNode = soundBank[track].createNode()
+    bufferNode.start(event.deadline)
+  }, nextBeatTime(beatInd))
+  event.repeat(barDur)
+  event.tolerance({late: 0.01})
+  beats[track][beatInd] = event
+}
+
+// This function deactivates the beat `beatInd` of `track`.
+var stopBeat = function(track, beatInd) {
+  var event = beats[track][beatInd]
+  event.clear()
+}
+
+// ---------- Just some helpers ---------- //
+// This helper calculates the absolute time of the upcoming `beatInd`. 
+var nextBeatTime = function(beatInd) {
+  var currentTime = context.currentTime
+    , currentBar = Math.floor(currentTime / barDur)
+    , currentBeat = Math.round(currentTime % barDur)
+  if (currentBeat < beatInd) return currentBar * barDur + beatInd * beatDur
+  else return (currentBar + 1) * barDur + beatInd * beatDur
+}
+
+// This helper loads sound buffers
+var loadTrack = function(track) {
+  var request = new XMLHttpRequest()
+  request.open('GET', 'sounds/' + track + '.wav', true)
+  request.responseType = 'arraybuffer'
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      var createNode = function() {
+        var node = context.createBufferSource()
+        node.buffer = buffer
+        node.connect(context.destination)
+        return node
+      }
+      soundBank[track] = { createNode: createNode }
+    })
+  }
+  request.send()
+}
